c5de1ea69fa2e153cbf7140fbae9e80ea4ec5f8b
Aleksi1212 <aleksinoro@gmail.com>
2023-06-14 16:47:31 +0300

    route for logging in

---
+++ src/routes/logIn/index.tsx
@@ -0,0 +1,15 @@
+import { component$ } from "@builder.io/qwik";
+import { DocumentHead, Form } from "@builder.io/qwik-city";
+
+
+export default component$(() => {
+    return (
+        <>
+            <h1>log in</h1>
+        </>
+    )
+})
+
+export const head: DocumentHead = {
+    title: 'Videoli - Log in'
+}
