9edad420de032185e21f9ecf53ba6f5bde7712bd
Aleksi1212 <aleksinoro@gmail.com>
2023-06-14 16:52:04 +0300

    fix deployment

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,4 @@
 import { component$, Slot, useStyles$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
 import type { RequestHandler } from '@builder.io/qwik-city';
 
 import styles from './styles.css?inline';
