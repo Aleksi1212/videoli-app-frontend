9fb7295ab2442988eb1a67d008715c0e7c8f31fc
Aleksi1212 <aleksinoro@gmail.com>
2023-06-18 15:52:10 +0300

    daisyUI and custom colors/themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}",],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        colors: {
+            textColor: '#ebf7fe',
+            background: '#010c14',
+            primary_button: '#384ffa',
+            secondary_button: '#101c44',
+            accent: '#4f05c7',
+        },
+        extend: {},
+    },
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: '#384ffa',
+                    secondary: '#101c44',
+                    accent: '#4f05c7',
+                },
+            },
+        ],
+    },
+    plugins: [require('daisyui')],
+};
