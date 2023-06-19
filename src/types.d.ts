25c02737eae964cf84b497a3b30412588751901c
Aleksi1212 <aleksinoro@gmail.com>
2023-06-19 14:32:24 +0300

    updated WaterMarkStyleTypes

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,14 +1,17 @@
 interface MediaTypes {
-    video: string
-    waterMark: string
+    video: string;
+    waterMark: string;
 }
 
 interface WaterMarkStyleValues {
     value: number;
     max: number;
+    header: string;
 }
 
 interface WaterMarkStyleTypes {
+    xPos: WaterMarkStyleValues;
+    yPos: WaterMarkStyleValues;
     size: WaterMarkStyleValues;
     opacity: WaterMarkStyleValues;
     rotation: WaterMarkStyleValues;
