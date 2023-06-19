77574df590ddbd21c07ae80afefaca33078863d4
Aleksi1212 <aleksinoro@gmail.com>
2023-06-19 14:33:34 +0300

    added header component and updated document head

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import { VideoEditor } from '~/components/editor/videoEditor';
+import { MainHeader } from '~/components/navigation/mainHeader';
 import { type DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
@@ -8,10 +9,18 @@ export default component$(() => {
             <div class="w-full h-[100svh] flex justify-center items-center">
                 <VideoEditor />
             </div>
+
+            <MainHeader />
         </>
     );
 });
 
 export const head: DocumentHead = {
     title: 'Videoli',
+    meta: [
+        {
+            name: 'Watermark to video generator',
+            content: 'Add watermarks to videos using videoli',
+        },
+    ],
 };
