c8982bc50babb9c67b5eefaebebdf6b8cfd11fcb
Aleksi1212 <aleksinoro@gmail.com>
2023-06-20 14:28:40 +0300

    fixed header buttons

--- src/components/navigation/mainHeader.tsx
+++ src/components/navigation/mainHeader.tsx
@@ -2,28 +2,51 @@ import { component$ } from '@builder.io/qwik';
 import { Link } from '@builder.io/qwik-city';
 import { Image } from '@unpic/qwik';
 
-import star from '../../icons/star.webp'
-import coffee from '../../icons/coffee.webp'
+import star from '~/icons/star.webp';
+import coffee from '~/icons/coffee.webp';
 
 export const MainHeader = component$(() => {
     return (
-        <header class="w-full flex p-5 fixed justify-between header top-0">
+        <header class="w-full flex p-5 fixed justify-between top-0 header">
             <h1 class="text-5xl text-[white]">Videoli</h1>
 
             <div class="flex gap-3 items-center">
                 <Link href="/logIn" class="mainHeaderButton bg-primary_button">
-                    Sign up
+                    <p>Sign up</p>
                 </Link>
 
                 <div class="h-full border-l"></div>
 
-                <a href="https://github.com/Aleksi1212/videoli-app-frontend" target="_blank" class="mainHeaderButton headerGlassyButton flex gap-2">
+                <a
+                    href="https://github.com/Aleksi1212/videoli-app-frontend"
+                    target="_blank"
+                    class="mainHeaderButton headerGlassyButton"
+                >
                     <p>Star on github</p>
-                    <Image src={star} alt="star" width={24} height={24} loading="lazy" />
+                    <div>
+                        <Image
+                            src={star}
+                            alt="star"
+                            width={24}
+                            height={24}
+                            loading="lazy"
+                        />
+                    </div>
                 </a>
-                <Link href="/coffee" class="mainHeaderButton headerGlassyButton flex gap-2">
+                <Link
+                    href="/coffee"
+                    class="mainHeaderButton headerGlassyButton"
+                >
                     <p>Buy me coffee</p>
-                    <Image src={coffee} alt="coffee" width={24} height={24} loading="lazy" />
+                    <div>
+                        <Image
+                            src={coffee}
+                            alt="coffee"
+                            width={24}
+                            height={24}
+                            loading="lazy"
+                        />
+                    </div>
                 </Link>
             </div>
         </header>
