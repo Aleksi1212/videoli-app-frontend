c71b55039b1bc3c40eda303e9a95fd0369d4146c
Aleksi1212 <aleksinoro@gmail.com>
2023-06-21 14:54:49 +0300

    logIn index.tsx

--- src/routes/logIn/index.tsx
+++ src/routes/logIn/index.tsx
@@ -1,15 +1,33 @@
-import { component$ } from "@builder.io/qwik";
-import { type DocumentHead } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik';
+import { routeLoader$, z, type DocumentHead } from '@builder.io/qwik-city';
+import type { InitialValues } from '@modular-forms/qwik';
 
+import { AuthenticationBox } from '~/components/authentication/authenticationBox';
+
+const logInSchema = z.object({
+    email: z.string().min(1, 'Please enter your email').email(),
+});
+type LogInForm = z.infer<typeof logInSchema>;
+
+export const useLogInLoader = routeLoader$<InitialValues<LogInForm>>(() => ({
+    email: '',
+}));
 
 export default component$(() => {
     return (
-        <>
-            <h1>log in</h1>
-        </>
-    )
-})
+        <section class="w-full h-[100svh] flex justify-center">
+            <AuthenticationBox />
+        </section>
+    );
+});
 
 export const head: DocumentHead = {
-    title: 'Videoli - Log in'
-}
+    title: 'Videoli - log in',
+    meta: [
+        {
+            name: 'Log in to your videoli account',
+            content:
+                'Log in to your videoli account to get access to a cool editor',
+        },
+    ],
+};
