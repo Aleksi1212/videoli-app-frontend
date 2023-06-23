2ba9cdc9c5fe261d052a685f8b8d9d06ae266e61
Aleksi1212 <aleksinoro@gmail.com>
2023-06-23 13:46:51 +0300

    ui for authentication code box

---
+++ src/components/authentication/authenticationCodeBox.tsx
@@ -0,0 +1,72 @@
+import { component$, useSignal, useVisibleTask$ } from '@builder.io/qwik';
+import {
+    authenticationCodeSchema,
+    type AuthenticationCodeForm,
+} from '~/dataHandler/schemas';
+import { useForm, zodForm$ } from '@modular-forms/qwik';
+import { useAuthenticationCodeLoader } from '~/routes/signIn/authenticationCode';
+
+import {
+    AuthenticationBoxTitle,
+    AuthenticationBoxImage,
+} from './authenticationBoxContent';
+
+export const AuthenticationCodeBox = component$(() => {
+    const [, { Form, Field }] = useForm<AuthenticationCodeForm>({
+        loader: useAuthenticationCodeLoader(),
+        validate: zodForm$(authenticationCodeSchema)
+    })
+
+    const loading = useSignal<boolean>(true)
+    const sentToEmail = useSignal<string>('')
+
+    useVisibleTask$(() => {
+        const params = new URLSearchParams(window.location.search)
+        sentToEmail.value = params.get('sentTo') || ''
+        loading.value = false
+    })
+
+    if (loading.value) {
+        return <span class="loading loading-spinner w-[5rem]"></span>;
+    }
+
+    return (
+        <>
+            <div class="w-[28rem] h-[32rem] mt-24 bg-secondary_button rounded-xl flex flex-col justify-evenly items-center">
+                <AuthenticationBoxImage />
+
+                <div class="w-full px-10 gap-5 flex flex-col">
+                    <AuthenticationBoxTitle authStep="code" email={sentToEmail.value} />
+
+                    <Form class="flex flex-col justify-center text-textColor gap-3">
+                        <Field name="code">
+                            {(field, props) => (
+                                <div class="w-full relative">
+                                    <input
+                                        {...props}
+                                        type="code"
+                                        value={field.value}
+                                        placeholder="Enter acces code"
+                                        class="w-full rounded-lg bg-background text-xl p-2 focus:outline-none"
+                                        maxLength={6}
+                                    />
+                                    {field.error && (
+                                        <h1 class="text-lg text-[#ED2939] underline absolute pl-1 top-[-2rem] font-[500]">
+                                            {field.error}
+                                        </h1>
+                                    )}
+                                </div>
+                            )}
+                        </Field>
+                        <button
+                            type="submit"
+                            class="w-full bg-primary_button rounded-lg text-xl py-2 hover:brightness-75 active:scale-95"
+                        >
+                            Sign In
+                        </button>
+                    </Form>
+                </div>
+            </div>
+        </>
+    );
+});
