ace1417476eb25f10b3c605660b8661fe8abd356
Aleksi1212 <aleksinoro@gmail.com>
2023-07-04 20:40:17 +0300

    queryParamTypeConverter

---
+++ src/utils/queryParamTypeConversion.ts
@@ -0,0 +1,32 @@
+type QueryParamMapping = {
+    true: boolean;
+    false: boolean;
+    null: null;
+    [string: string]: any;
+};
+
+type ConvertQueryParamValue<TQueryParam extends keyof QueryParamMapping> =
+    TQueryParam extends keyof QueryParamMapping
+        ? QueryParamMapping[TQueryParam]
+        : string;
+
+function convertQueryParam<TQueryParam extends keyof QueryParamMapping>(
+    queryParam: string
+): ConvertQueryParamValue<TQueryParam> {
+    const valueMappings: QueryParamMapping = {
+        true: true,
+        false: false,
+        null: null,
+        string: queryParam,
+    };
+
+    if (queryParam in valueMappings) {
+        return valueMappings[
+            queryParam as TQueryParam
+        ] as ConvertQueryParamValue<TQueryParam>;
+    } else {
+        return queryParam as ConvertQueryParamValue<TQueryParam>;
+    }
+}
+
+export { convertQueryParam };
