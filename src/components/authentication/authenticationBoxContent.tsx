adba8f05fbb79cca60a2f5451e798a3177bf5494
Aleksi1212 <aleksinoro@gmail.com>
2023-07-08 15:16:53 +0300

    prettier, and manage props better

--- src/components/authentication/authenticationBoxContent.tsx
+++ src/components/authentication/authenticationBoxContent.tsx
@@ -2,42 +2,51 @@ import { component$ } from '@builder.io/qwik';
 import { Image } from '@unpic/qwik';
 import logo2 from '~/icons/logo2.svg';
 
-export const AuthenticationBoxTitle = component$(
-    (props: { authStep: string; email?: string }) => {
-        const content: AuthContent = {
-            title: 'Sign In',
-            content:
-                '<p>By signing in you get access to a advanced editor with more features.</p>',
-            contentStyles: 'text-center text-sm opacity-75',
-        };
+interface AuthBoxTitleProps {
+    authStep: string;
+    email?: string;
+}
 
-        if (props.authStep === 'code') {
-            content.title = 'Enter Code';
-            content.content = `<p>An access-code has been sent to <span class="text-[#1cb2f5]">${props.email}</span>. Please allow N minutes for the code to arrive, and check any spam/junk folders.</p>`;
-            content.contentStyles =
-                'text-center text-xs opacity-75 bg-background border border-primary_button p-2 rounded-lg';
-        }
+export const AuthenticationBoxTitle = component$<AuthBoxTitleProps>((props) => {
+    const content: AuthContent = {
+        title: 'Sign In',
+        content:
+            '<p>By signing in you get access to a advanced editor with more features.</p>',
+        contentStyles: 'text-center text-sm opacity-75',
+    };
 
-        return (
-            <>
-                <div class="flex flex-col gap-1 items-center">
-                    <h1 class="text-4xl text-textColor underline">
-                        {content.title}
-                    </h1>
-                    <div
-                        class={content.contentStyles}
-                        dangerouslySetInnerHTML={content.content}
-                    ></div>
-                </div>
-            </>
-        );
+    if (props.authStep === 'code') {
+        content.title = 'Enter Code';
+        content.content = `<p>An access-code has been sent to <span class="text-[#1cb2f5]">${props.email}</span>. Please allow N minutes for the code to arrive, and check any spam/junk folders.</p>`;
+        content.contentStyles =
+            'text-center text-xs opacity-75 bg-background border border-primary_button p-2 rounded-lg';
     }
-);
+
+    return (
+        <>
+            <div class="flex flex-col gap-1 items-center">
+                <h1 class="text-4xl text-textColor underline">
+                    {content.title}
+                </h1>
+                <div
+                    class={content.contentStyles}
+                    dangerouslySetInnerHTML={content.content}
+                ></div>
+            </div>
+        </>
+    );
+});
 
 export const AuthenticationBoxImage = component$(() => {
     return (
         <div class="flex flex-col justify-center items-center gap-1 w-full h-full">
-            <Image src={logo2} alt="logo2" width={250} height={250} loading="lazy" />
+            <Image
+                src={logo2}
+                alt="logo2"
+                width={250}
+                height={250}
+                loading="lazy"
+            />
         </div>
     );
 });
